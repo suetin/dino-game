cb3822c8e849c917999e79a90c9342f25b05e539
Richbanker <123333089+Richbanker@users.noreply.github.com>
2026-01-28 00:10:59 +0300

    test(client): fix jest config with setup file and globals
    - Add jest.setup.js to define global variables via global object
    
    - Add __EXTERNAL_SERVER_URL__ and __INTERNAL_SERVER_URL__ to globals config
    
    - Add nx.json with useShell option for Lerna/Nx tasks

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -3,15 +3,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// Define global variables for Jest
-global.__EXTERNAL_SERVER_URL__ = 'http://localhost:3001';
-global.__INTERNAL_SERVER_URL__ = 'http://localhost:3001';
-
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  setupFiles: ['<rootDir>/jest.setup.js'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
+    __EXTERNAL_SERVER_URL__: 'http://localhost:3001',
+    __INTERNAL_SERVER_URL__: 'http://localhost:3001',
   },
 };
---
+++ packages/client/jest.setup.js
@@ -0,0 +1,4 @@
+/* eslint-disable */
+// Setup file for Jest to define global variables
+global.__EXTERNAL_SERVER_URL__ = 'http://localhost:3001';
+global.__INTERNAL_SERVER_URL__ = 'http://localhost:3001';
