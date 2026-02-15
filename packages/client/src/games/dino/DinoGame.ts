a9eef27190e105ec875ac45c7910f993b6f70e6f
Alexander Suetin <asuetin@gpmd.ru>
2026-02-16 00:52:02 +0800

    feat: Refactoring game

--- packages/client/src/games/dino/DinoGame.ts
+++ packages/client/src/games/dino/DinoGame.ts
@@ -1,15 +1,49 @@
 import {
+  CACTUS_HITBOX_INSET_BOTTOM,
+  CACTUS_HITBOX_INSET_LEFT,
+  CACTUS_HITBOX_INSET_RIGHT,
+  CACTUS_HITBOX_INSET_TOP,
+  DINO_ANIMATION_FPS,
+  DINO_DEAD_FRAME_INDEX,
+  DINO_DEAD_SPRITE_SOURCE_WIDTH,
   DINO_HEIGHT,
+  DINO_HITBOX_INSET_BOTTOM,
+  DINO_HITBOX_INSET_LEFT,
+  DINO_HITBOX_INSET_RIGHT,
+  DINO_HITBOX_INSET_TOP,
+  DINO_JUMP_FRAME_INDEX,
+  DINO_RUN_FRAME_INDICES,
+  DINO_SPRITE_FIRST_FRAME_Y,
+  DINO_SPRITE_FRAME_HEIGHT,
+  DINO_SPRITE_FRAME_STRIDE_Y,
+  DINO_SPRITE_SOURCE_WIDTH,
+  DINO_SPRITE_SOURCE_X,
   DINO_WIDTH,
   GAME_HEIGHT,
   GAME_WIDTH,
   GRAVITY,
-  GROUND_Y,
   JUMP_VELOCITY,
+  UI_TEXT_COLOR,
+  UI_TEXT_SCALE,
   WORLD_SPEED,
 } from './constants'
 import { Cactus, createCactus, createDino, Dino } from './entities'
 import { createInputState } from './input'
+import mitt, { Emitter } from 'mitt'
+import dinoSpriteUrl from '../../assets/dino_sprite.png'
+import cactusSpriteUrl from '../../assets/cactus_1.png'
+
+type DinoGameEvents = {
+  score: number
+  gameover: number
+}
+
+type Rect = {
+  left: number
+  right: number
+  top: number
+  bottom: number
+}
 
 export type DinoGameOptions = {
   width?: number
@@ -19,24 +53,42 @@ export type DinoGameOptions = {
 export class DinoGame {
   private readonly ctx: CanvasRenderingContext2D
   private readonly input = createInputState()
+  private readonly emitter: Emitter<DinoGameEvents> = mitt<DinoGameEvents>()
+
   private width: number
   private height: number
   private lastTime = 0
   private rafId = 0
 
-  private dino: Dino = createDino()
-  private cactus: Cactus = createCactus(GAME_WIDTH + 200)
+  private dino: Dino
+  private cactus: Cactus
   private score = 0
   private isRunning = false
+  private isGameOver = false
+  private lastEmittedScore = -1
+
+  private readonly dinoSprite = new Image()
+  private isDinoSpriteReady = false
+  private readonly cactusSprite = new Image()
+  private isCactusSpriteReady = false
+
+  private runAnimationElapsed = 0
+  private runFrameCursor = 0
 
   constructor(ctx: CanvasRenderingContext2D, options: DinoGameOptions = {}) {
     this.ctx = ctx
     this.width = options.width ?? GAME_WIDTH
     this.height = options.height ?? GAME_HEIGHT
+
+    this.dino = createDino(this.getGroundY())
+    this.cactus = createCactus(this.width + 200, this.getGroundY())
+
+    this.setupSprites()
   }
 
   public start() {
     if (this.isRunning) return
+
     this.isRunning = true
     this.lastTime = performance.now()
     this.loop(this.lastTime)
@@ -47,19 +99,83 @@ export class DinoGame {
     cancelAnimationFrame(this.rafId)
   }
 
+  public reset() {
+    this.resetWorld()
+    this.emitScore()
+  }
+
+  public resize(width: number, height: number) {
+    const oldGroundY = this.getGroundY()
+    const dinoOffsetFromGround = oldGroundY - this.dino.position.y
+
+    this.width = width
+    this.height = height
+
+    const newGroundY = this.getGroundY()
+    this.dino.position.y = newGroundY - dinoOffsetFromGround
+
+    if (this.dino.position.y >= newGroundY - DINO_HEIGHT) {
+      this.dino.position.y = newGroundY - DINO_HEIGHT
+      this.dino.velocityY = 0
+      this.dino.isOnGround = true
+    }
+
+    this.cactus.position.y = newGroundY - this.cactus.height
+  }
+
   public requestJump() {
     this.input.jumpRequested = true
   }
 
-  public reset() {
-    this.dino = createDino()
-    this.cactus = createCactus(this.width + 200)
-    this.score = 0
+  public renderStartHint() {
+    this.render(true)
   }
 
-  public resize(width: number, height: number) {
-    this.width = width
-    this.height = height
+  public on<Event extends keyof DinoGameEvents>(
+    type: Event,
+    handler: (event: DinoGameEvents[Event]) => void
+  ) {
+    this.emitter.on(type, handler)
+  }
+
+  public off<Event extends keyof DinoGameEvents>(
+    type: Event,
+    handler: (event: DinoGameEvents[Event]) => void
+  ) {
+    this.emitter.off(type, handler)
+  }
+
+  private setupSprites() {
+    this.loadSprite(this.dinoSprite, dinoSpriteUrl, () => {
+      this.isDinoSpriteReady = true
+    })
+
+    this.loadSprite(this.cactusSprite, cactusSpriteUrl, () => {
+      this.isCactusSpriteReady = true
+    })
+  }
+
+  private loadSprite(image: HTMLImageElement, url: string, onLoad: () => void) {
+    image.onload = () => {
+      onLoad()
+      if (!this.isRunning) {
+        this.renderStartHint()
+      }
+    }
+    image.onerror = () => {
+      console.error('Не удалось загрузить спрайт:', url)
+    }
+    image.src = url
+  }
+
+  private resetWorld() {
+    const groundY = this.getGroundY()
+    this.dino = createDino(groundY)
+    this.cactus = createCactus(this.width + 200, groundY)
+    this.score = 0
+    this.isGameOver = false
+    this.runAnimationElapsed = 0
+    this.runFrameCursor = 0
   }
 
   private loop = (time: number) => {
@@ -67,7 +183,7 @@ export class DinoGame {
     this.lastTime = time
 
     this.update(delta)
-    this.render()
+    this.render(false)
 
     if (this.isRunning) {
       this.rafId = requestAnimationFrame(this.loop)
@@ -75,55 +191,250 @@ export class DinoGame {
   }
 
   private update(delta: number) {
+    if (this.isGameOver) return
+
+    const groundY = this.getGroundY()
+
+    this.updateScore(delta)
+    this.consumeJumpInput()
+    this.updateDino(delta, groundY)
+    this.updateCactus(delta, groundY)
+
+    if (this.checkCollision()) {
+      this.isGameOver = true
+      this.emitter.emit('gameover', Math.floor(this.score))
+      return
+    }
+
+    this.updateRunAnimation(delta)
+  }
+
+  private updateScore(delta: number) {
+    this.score += delta * 10
+    this.emitScore()
+  }
+
+  private consumeJumpInput() {
     if (this.input.jumpRequested && this.dino.isOnGround) {
       this.dino.velocityY = -JUMP_VELOCITY
       this.dino.isOnGround = false
     }
     this.input.jumpRequested = false
+  }
 
+  private updateDino(delta: number, groundY: number) {
     this.dino.velocityY += GRAVITY * delta
     this.dino.position.y += this.dino.velocityY * delta
 
-    if (this.dino.position.y >= GROUND_Y - DINO_HEIGHT) {
-      this.dino.position.y = GROUND_Y - DINO_HEIGHT
+    if (this.dino.position.y >= groundY - DINO_HEIGHT) {
+      this.dino.position.y = groundY - DINO_HEIGHT
       this.dino.velocityY = 0
       this.dino.isOnGround = true
     }
+  }
 
+  private updateCactus(delta: number, groundY: number) {
     this.cactus.position.x -= WORLD_SPEED * delta
-    if (this.cactus.position.x + this.cactus.width < 0) {
-      this.cactus = createCactus(this.width + 150 + Math.random() * 200)
-      this.score += 1
+
+    if (this.cactus.position.x + this.cactus.width >= 0) {
+      return
+    }
+
+    this.cactus = createCactus(this.width + 150 + Math.random() * 200, groundY)
+    this.score += 1
+    this.emitScore()
+  }
+
+  private checkCollision() {
+    const dino = this.getDinoHitbox()
+    const cactus = this.getCactusHitbox()
+
+    return (
+      dino.left < cactus.right &&
+      dino.right > cactus.left &&
+      dino.top < cactus.bottom &&
+      dino.bottom > cactus.top
+    )
+  }
+
+  private getDinoHitbox(): Rect {
+    return {
+      left: this.dino.position.x + DINO_WIDTH * DINO_HITBOX_INSET_LEFT,
+      right: this.dino.position.x + DINO_WIDTH * (1 - DINO_HITBOX_INSET_RIGHT),
+      top: this.dino.position.y + DINO_HEIGHT * DINO_HITBOX_INSET_TOP,
+      bottom: this.dino.position.y + DINO_HEIGHT * (1 - DINO_HITBOX_INSET_BOTTOM),
     }
   }
 
-  private render() {
-    const ctx = this.ctx
-    ctx.clearRect(0, 0, this.width, this.height)
+  private getCactusHitbox(): Rect {
+    return {
+      left: this.cactus.position.x + this.cactus.width * CACTUS_HITBOX_INSET_LEFT,
+      right: this.cactus.position.x + this.cactus.width * (1 - CACTUS_HITBOX_INSET_RIGHT),
+      top: this.cactus.position.y + this.cactus.height * CACTUS_HITBOX_INSET_TOP,
+      bottom: this.cactus.position.y + this.cactus.height * (1 - CACTUS_HITBOX_INSET_BOTTOM),
+    }
+  }
 
+  private emitScore() {
+    const roundedScore = Math.floor(this.score)
+    if (roundedScore === this.lastEmittedScore) return
+
+    this.lastEmittedScore = roundedScore
+    this.emitter.emit('score', roundedScore)
+  }
+
+  private render(showStartHint: boolean) {
+    const groundY = this.getGroundY()
+
+    this.renderBackground(groundY)
+    this.renderDino()
+    this.renderCactus()
+    this.renderHud()
+    this.renderOverlay(showStartHint)
+  }
+
+  private renderBackground(groundY: number) {
+    const ctx = this.ctx
+
+    ctx.clearRect(0, 0, this.width, this.height)
     ctx.fillStyle = '#f7f5f0'
     ctx.fillRect(0, 0, this.width, this.height)
 
     ctx.strokeStyle = '#3b3b3b'
     ctx.lineWidth = 2
     ctx.beginPath()
-    ctx.moveTo(0, GROUND_Y)
-    ctx.lineTo(this.width, GROUND_Y)
+    ctx.moveTo(0, groundY)
+    ctx.lineTo(this.width, groundY)
     ctx.stroke()
+  }
 
-    ctx.fillStyle = '#2f2f2f'
-    ctx.fillRect(this.dino.position.x, this.dino.position.y, DINO_WIDTH, DINO_HEIGHT)
+  private renderHud() {
+    const ctx = this.ctx
 
-    ctx.fillStyle = '#0e8a5a'
-    ctx.fillRect(
+    ctx.fillStyle = UI_TEXT_COLOR
+    ctx.font = `${16 * UI_TEXT_SCALE}px monospace`
+    ctx.fillText(`Очки: ${Math.floor(this.score)}`, 16, 24 * UI_TEXT_SCALE)
+  }
+
+  private renderOverlay(showStartHint: boolean) {
+    const ctx = this.ctx
+
+    if (this.isGameOver) {
+      ctx.fillStyle = UI_TEXT_COLOR
+      ctx.textAlign = 'center'
+      ctx.font = `${24 * UI_TEXT_SCALE}px monospace`
+      ctx.fillText('Конец игры', this.width / 2, this.height / 2)
+      ctx.font = `${14 * UI_TEXT_SCALE}px monospace`
+      ctx.fillText('Нажмите R для рестарта', this.width / 2, this.height / 2 + 24 * UI_TEXT_SCALE)
+      ctx.textAlign = 'start'
+      return
+    }
+
+    if (!showStartHint) {
+      return
+    }
+
+    ctx.fillStyle = UI_TEXT_COLOR
+    ctx.textAlign = 'center'
+    ctx.font = `${14 * UI_TEXT_SCALE}px monospace`
+    ctx.fillText('Кликни или нажми Пробел', this.width / 2, this.height / 2)
+    ctx.textAlign = 'start'
+  }
+
+  private getGroundY() {
+    return Math.max(DINO_HEIGHT + 20, this.height - 60)
+  }
+
+  private updateRunAnimation(delta: number) {
+    if (!this.dino.isOnGround) return
+
+    this.runAnimationElapsed += delta
+    const frameDuration = 1 / DINO_ANIMATION_FPS
+
+    while (this.runAnimationElapsed >= frameDuration) {
+      this.runAnimationElapsed -= frameDuration
+      this.runFrameCursor = (this.runFrameCursor + 1) % DINO_RUN_FRAME_INDICES.length
+    }
+  }
+
+  private getCurrentDinoFrameIndex() {
+    if (this.isGameOver) {
+      return DINO_DEAD_FRAME_INDEX
+    }
+
+    if (!this.dino.isOnGround) {
+      return DINO_JUMP_FRAME_INDEX
+    }
+
+    return DINO_RUN_FRAME_INDICES[this.runFrameCursor]
+  }
+
+  private renderDino() {
+    const ctx = this.ctx
+
+    if (!this.isDinoSpriteReady) {
+      ctx.fillStyle = '#2f2f2f'
+      ctx.fillRect(this.dino.position.x, this.dino.position.y, DINO_WIDTH, DINO_HEIGHT)
+      return
+    }
+
+    const frameIndex = this.getCurrentDinoFrameIndex()
+    const sourceX = DINO_SPRITE_SOURCE_X
+    const sourceY = DINO_SPRITE_FIRST_FRAME_Y + frameIndex * DINO_SPRITE_FRAME_STRIDE_Y
+    const sourceHeight = DINO_SPRITE_FRAME_HEIGHT
+    const maxSourceWidth =
+      frameIndex === DINO_DEAD_FRAME_INDEX
+        ? DINO_DEAD_SPRITE_SOURCE_WIDTH
+        : DINO_SPRITE_SOURCE_WIDTH
+    const sourceWidth = Math.min(maxSourceWidth, this.dinoSprite.width - sourceX)
+
+    if (sourceWidth <= 0 || sourceY + sourceHeight > this.dinoSprite.height) {
+      return
+    }
+
+    ctx.save()
+    ctx.imageSmoothingEnabled = false
+    ctx.drawImage(
+      this.dinoSprite,
+      sourceX,
+      sourceY,
+      sourceWidth,
+      sourceHeight,
+      this.dino.position.x,
+      this.dino.position.y,
+      DINO_WIDTH,
+      DINO_HEIGHT
+    )
+    ctx.restore()
+  }
+
+  private renderCactus() {
+    const ctx = this.ctx
+
+    if (!this.isCactusSpriteReady) {
+      ctx.fillStyle = '#0e8a5a'
+      ctx.fillRect(
+        this.cactus.position.x,
+        this.cactus.position.y,
+        this.cactus.width,
+        this.cactus.height
+      )
+      return
+    }
+
+    ctx.save()
+    ctx.imageSmoothingEnabled = false
+    ctx.drawImage(
+      this.cactusSprite,
+      0,
+      0,
+      this.cactusSprite.width,
+      this.cactusSprite.height,
       this.cactus.position.x,
       this.cactus.position.y,
       this.cactus.width,
       this.cactus.height
     )
-
-    ctx.fillStyle = '#1a1a1a'
-    ctx.font = '16px monospace'
-    ctx.fillText(`Очки: ${this.score}`, 16, 24)
+    ctx.restore()
   }
 }
--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { usePage } from '@/hooks/usePage'
 import { DinoGame } from '@/games/dino/DinoGame'
 import { GAME_HEIGHT, GAME_WIDTH } from '@/games/dino/constants'
@@ -6,42 +6,156 @@ import '../../styles/DinoGame.css'
 import { WrapperContent } from '@/components/WrapperContent'
 import { PageMeta } from '@/components/PageMeta'
 
+const MIN_CANVAS_WIDTH = 320
+const MIN_CANVAS_HEIGHT = 240
+
+const getCanvasSize = (element: HTMLElement) => {
+  const rect = element.getBoundingClientRect()
+  return {
+    width: Math.max(MIN_CANVAS_WIDTH, Math.floor(rect.width || GAME_WIDTH)),
+    height: Math.max(MIN_CANVAS_HEIGHT, Math.floor(rect.height || GAME_HEIGHT)),
+  }
+}
+
+const isTypingTarget = (target: EventTarget | null) => {
+  if (!(target instanceof HTMLElement)) {
+    return false
+  }
+
+  const tagName = target.tagName
+  return (
+    tagName === 'INPUT' ||
+    tagName === 'TEXTAREA' ||
+    tagName === 'SELECT' ||
+    target.isContentEditable
+  )
+}
+
 export const GamePage = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  const canvasWrapRef = useRef<HTMLDivElement | null>(null)
 
   usePage({ initPage: initGamePage })
 
   useEffect(() => {
     const canvas = canvasRef.current
-    if (!canvas) return
+    const canvasWrap = canvasWrapRef.current
+    if (!canvas || !canvasWrap) return
 
     const ctx = canvas.getContext('2d')
     if (!ctx) return
 
-    const game = new DinoGame(ctx, {
-      width: canvas.width,
-      height: canvas.height,
-    })
+    const applyCanvasResolution = (width: number, height: number) => {
+      const dpr = window.devicePixelRatio || 1
+      canvas.width = Math.floor(width * dpr)
+      canvas.height = Math.floor(height * dpr)
+      ctx.setTransform(dpr, 0, 0, dpr, 0, 0)
+    }
 
-    const onClick = () => game.requestJump()
-    const onKeyDown = (event: KeyboardEvent) => {
-      if (event.code === 'Space' || event.code === 'ArrowUp') {
-        game.requestJump()
+    const initialSize = getCanvasSize(canvasWrap)
+    applyCanvasResolution(initialSize.width, initialSize.height)
+
+    const game = new DinoGame(ctx, {
+      width: initialSize.width,
+      height: initialSize.height,
+    })
+    game.renderStartHint()
+
+    let hasStarted = false
+    let isGameOver = false
+
+    const onGameOver = (score: number) => {
+      isGameOver = true
+      console.log('Игра закончилась. Очки:', score)
+    }
+
+    const startGame = () => {
+      hasStarted = true
+      console.log('Игра началась')
+      game.start()
+    }
+
+    const restartGame = () => {
+      isGameOver = false
+      game.reset()
+      game.start()
+    }
+
+    const requestJump = () => {
+      if (!hasStarted || isGameOver) return
+      game.requestJump()
+    }
+
+    const handlePrimaryAction = () => {
+      if (!hasStarted) {
+        startGame()
+        return
       }
-      if (event.code === 'KeyR') {
-        game.reset()
+      if (isGameOver) {
+        restartGame()
+        return
+      }
+      requestJump()
+    }
+
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (isTypingTarget(event.target)) {
+        return
+      }
+
+      if (event.repeat) {
+        return
+      }
+
+      switch (event.code) {
+        case 'Space':
+          event.preventDefault()
+          handlePrimaryAction()
+          return
+        case 'ArrowUp':
+          event.preventDefault()
+          requestJump()
+          return
+        case 'KeyR':
+          if (isGameOver) {
+            restartGame()
+          }
+          return
+        default:
+          return
       }
     }
 
-    canvas.addEventListener('pointerdown', onClick)
-    window.addEventListener('keydown', onKeyDown)
+    const syncCanvasSize = () => {
+      const nextSize = getCanvasSize(canvasWrap)
+      const nextWidth = nextSize.width
+      const nextHeight = nextSize.height
 
-    game.start()
+      if (canvas.clientWidth === nextWidth && canvas.clientHeight === nextHeight) {
+        return
+      }
+
+      applyCanvasResolution(nextWidth, nextHeight)
+      game.resize(nextWidth, nextHeight)
+      if (!hasStarted) {
+        game.renderStartHint()
+      }
+    }
+
+    const resizeObserver = new ResizeObserver(syncCanvasSize)
+    resizeObserver.observe(canvasWrap)
+    syncCanvasSize()
+
+    canvas.addEventListener('pointerdown', handlePrimaryAction)
+    window.addEventListener('keydown', onKeyDown)
+    game.on('gameover', onGameOver)
 
     return () => {
       game.stop()
-      canvas.removeEventListener('pointerdown', onClick)
+      resizeObserver.disconnect()
+      canvas.removeEventListener('pointerdown', handlePrimaryAction)
       window.removeEventListener('keydown', onKeyDown)
+      game.off('gameover', onGameOver)
     }
   }, [])
 
@@ -49,7 +163,7 @@ export const GamePage = () => {
     <WrapperContent className="max-w-[600px] items-center justify-start text-center">
       <PageMeta title="Dino Game" description="Дино" />
       <div className="dino-page">
-        <div className="dino-canvas-wrap">
+        <div ref={canvasWrapRef} className="dino-canvas-wrap">
           <canvas ref={canvasRef} width={GAME_WIDTH} height={GAME_HEIGHT} className="dino-canvas" />
         </div>
       </div>
