01129ebe835cd198825acea67a986235d885e9f9
Tarasova <v.tarasova@greendatasoft.ru>
2026-02-21 19:19:30 +0500

    fixed types

--- packages/client/src/games/dino/entities.ts
+++ packages/client/src/games/dino/entities.ts
@@ -1,14 +1,5 @@
 import { Vec2 } from './types'
-import {
-  BIRD_HEIGHT,
-  BIRD_SPRITE_FRAME_HEIGHT,
-  BIRD_SPRITE_WIDTH,
-  BIRD_WIDTH,
-  CACTUS_HEIGHT,
-  CACTUS_WIDTH,
-  CLOUD_Y_MAX,
-  DINO_HEIGHT,
-} from './constants'
+import { BIRD_HEIGHT, BIRD_WIDTH, DINO_HEIGHT } from './constants'
 
 export type Dino = {
   position: Vec2
@@ -16,23 +7,11 @@ export type Dino = {
   isOnGround: boolean
 }
 
-export type Cactus = {
-  position: Vec2
-  width: number
-  height: number
-}
-
-export type Bird = {
-  position: Vec2
-  width: number
-  height: number
-}
-
 export type ObstacleKind = 'cactus' | 'bird'
 
 export type ObstacleBase = {
   kind: ObstacleKind
-  position: { x: number; y: number }
+  position: Vec2
   width: number
   height: number
 }
@@ -47,7 +26,7 @@ export type BirdObstacle = ObstacleBase & { kind: 'bird' }
 export type Obstacle = CactusObstacle | BirdObstacle
 
 export type Cloud = {
-  position: { x: number; y: number }
+  position: Vec2
   width: number
   height: number
   scale?: number
@@ -58,27 +37,6 @@ export const createDino = (groundY: number): Dino => ({
   velocityY: 0,
   isOnGround: true,
 })
-
-export const createCactus = (x: number, groundY: number): Cactus => ({
-  position: { x, y: groundY - CACTUS_HEIGHT },
-  width: CACTUS_WIDTH,
-  height: CACTUS_HEIGHT,
-})
-
-export function createBird(x: number, groundY: number): Bird {
-  const height = BIRD_HEIGHT
-  const levels = [150, 190, 240]
-  const gap = levels[Math.floor(Math.random() * levels.length)]
-
-  return {
-    position: {
-      x,
-      y: groundY - height - gap,
-    },
-    width: BIRD_WIDTH,
-    height: BIRD_HEIGHT,
-  }
-}
 
 export function createCactusObstacle(
   x: number,
@@ -105,13 +63,5 @@ export function createBirdObstacle(x: number, groundY: number): BirdObstacle {
     position: { x, y: groundY - BIRD_HEIGHT - gap },
     width: BIRD_WIDTH,
     height: BIRD_HEIGHT,
-  }
-}
-
-export function createCloud(x: number): Cloud {
-  return {
-    position: { x, y: 0 },
-    width: 64,
-    height: CLOUD_Y_MAX,
   }
 }
