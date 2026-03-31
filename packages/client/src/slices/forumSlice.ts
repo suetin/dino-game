2f66f777a15889dcf64a739f04fc320dc2397dd3
OZi-13 <o_zi.cdr@mail.ru>
2026-03-31 12:09:08 +0530

    Feature/issue-53 API Forum Client review fix

--- packages/client/src/slices/forumSlice.ts
+++ packages/client/src/slices/forumSlice.ts
@@ -1,33 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState } from '@/store'
 import { SERVER_HOST2 } from '@/constants'
-
-export interface Topic {
-  id: number
-  title: string
-  description: string
-  author_id: string
-  createdAt: string
-  updatedAt: string
-}
-
-export interface Comment {
-  id: number
-  content: string
-  author_id: string
-  topic_id: number
-  parentId: number | null
-  createdAt: string
-  updatedAt: string
-  replies?: Comment[]
-}
-
-interface ForumState {
-  topics: Topic[]
-  currentComments: Comment[]
-  isLoading: boolean
-  error: string | null
-}
+import { Topic, Comment, ForumState } from './forum.types'
 
 const initialState: ForumState = {
   topics: [],
